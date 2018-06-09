9f5997dc0c5219a3b0eca6ba5ac5b9efae37b6f7
mohamedmaged <mohamedmaged35@yahoo.com>
2018-06-09 03:18:03 +0200

    test payload

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict'
 
-/***keys and tokens***/
+/***Global variables , keys and tokens***/
 let token ="EAAeGNZBbhP1wBALzadtj7X4a07UgEsUZB3oVojexl1qIdhIAsAZBhrNEsBLdlPEtBB6yUQKNCYdfs3kSWSbVcE6qRGxhNcwjbdHEI9STkyHIoDrWPn8w9M6ZCzm8ZCheGLQuFJAC1rkStQG24BRHDjHGuZCzMntUcneMtpr4YNy0AEHYwuWBLZC"
 var key= "83bcd41b2f4711fda30600152d4d32f1"
 var aikey= "e92f37fadbcf41bb86e74895bed5711d"
+let locData =  [
+		{
+			content_type : "location",
+			title : "get location",
+			payload : "location_for_developer"
+		}
+	]
 
 /***lib***/
 const express = require('express')
@@ -60,13 +67,28 @@ function sendText(sender, sText) {
 			recipient: {id: sender},
 			message :{
         text:sText ,
-        quick_replies : [
-          {
-						content_type : "location",
-						title : "get location",
-						payload : "location_for_developer"
-          }
-        ]
+      }
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log("sending error")
+		} else if (response.body.error) {
+			console.log("response body error")
+		}
+	})
+}
+
+function sendPayload(sender,data)
+{
+	request({
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs : {access_token: token},
+		method: "POST",
+		json: {
+			recipient: {id: sender},
+			message :{
+        text:sText ,
+        quick_replies : data
       }
 		}
 	}, function(error, response, body) {
@@ -87,11 +109,18 @@ function processText(sender,text)
   });
 
   req.on('response', function(response) {
+		if(response.result.intent.displayName === "weather")
+		{
     var city =response.result.parameters.city ;
     var latitude = response.result.parameters.lat;
     var longitude = response.result.parameters.long;
     var country = response.result.parameters.country;
     console.log(response.result.parameters);
+	}
+		if(!city || !latitude || !longitude || !country)
+		{
+			sendPayload(sender,locData)
+		}
     getTemperature(sender,city,country,latitude,longitude)
   });
   req.on('error', function(error) {
